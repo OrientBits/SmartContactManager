92b6d81770c27d60d4ed4bbcd8df97cd29e83eae
Ramshek <ramshekrana@gmail.com>
2022-11-13 21:55:32 +0530

    mapping on user add contact done!

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.smart.controller;
 
 import com.smart.dao.UserRepository;
+import com.smart.entities.Contact;
 import com.smart.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -30,5 +34,27 @@ public class UserController {
         return "normal/user_dashboard";
 
     }
+
+    //open add form handler
+
+    @GetMapping("/add-contact")
+    public String openAddContactForm(Model model){
+        model.addAttribute("title","Add Contact");
+        //model.addAttribute("contact",new Contact());
+
+        return "normal/add_contact_form";
+    }
+
+
+
+    @ModelAttribute
+    public void addCommonData(Model model,Principal principal){
+        String name = principal.getName();
+        System.out.println("USERNAME "+name);
+        //get the user by username
+        User user = repository.getUserByUserName(name);
+        model.addAttribute("user",user);
+    }
+
 
 }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,12 @@
+
 console.log("this is js script file")
+
+const toggleSidebar = () =>{
+    if ( $(".sidebar").is(":visible")){
+        $(".sidebar").css("display","none")
+        $(".content").css("margin-left","0%")
+    }else{
+        $(".sidebar").css("display","lock")
+        $(".content").css("margin-left","20%")
+    }
+}
