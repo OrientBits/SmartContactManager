e60966d850c31230a13463ee8772d099224a97d5
Ramshek <ramshekrana@gmail.com>
2022-11-14 22:17:21 +0530

    responsive for web done

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,11 +2,27 @@
 console.log("this is js script file")
 
 const toggleSidebar = () =>{
-    if ( $(".sidebar").is(":visible")){
-        $(".sidebar").css("display","none")
-        $(".content").css("margin-left","0%")
-    }else{
-        $(".sidebar").css("display","lock")
-        $(".content").css("margin-left","20%")
-    }
+
+    const sidebar = document.getElementById("main");
+    sidebar.classList.toggle('active');
+       
+   
+ 
+
+
+
 }
+
+
+/*
+// const openNav =  document.getElementById('openNav');
+// const closeNav =  document.getElementById('closeNav');
+
+const openNav =  document.querySelector('openNav');
+
+
+openNav.onclick = function (){
+    alert('this is workng');
+}
+
+*/
